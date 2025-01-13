f078139ed5d547b3aa87f00170f4da729aed427f
Christopher <chrisw09101994@gmail.com>
2025-01-13 02:11:46 +0000

    Adiciona data ao texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Obrigado por acreditar em mim, sei que quando nos sentimos sozinhos, oque mais queremos, no fundo, é alguém pra nos apoiar no maior momento de sombras que nos encontramos, e justamente, você quem me deu esse apoio e acreditou em mim quando eu mesmo não acreditei \n Obrigado</h1>
+    return <h1>Obrigado por acreditar em mim, sei que quando nos sentimos sozinhos, oque mais queremos, no fundo, é alguém pra nos apoiar no maior momento de sombras que nos encontramos, e justamente, você quem me deu esse apoio e acreditou em mim quando eu mesmo não acreditei \n Obrigado 12/01</h1>
 }
 
 export default Home;
