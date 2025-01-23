0faec3d290e1b4eebe5bd81ffc90b15f84becbcc
Christopher <chrisw09101994@gmail.com>
2025-01-23 01:25:36 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 function Home() {
-    return <h1>Obrigado por acreditar em mim, sei que quando nos sentimos sozinhos, oque mais queremos, no fundo, é alguém pra nos apoiar no maior momento de sombras que nos encontramos, e justamente, você quem me deu esse apoio e acreditou em mim quando eu mesmo não acreditei \n Obrigado 12/01</h1>
+  return (
+    <h1>
+      Obrigado por acreditar em mim, sei que quando nos sentimos sozinhos, oque
+      mais queremos, no fundo, é alguém pra nos apoiar no maior momento de
+      sombras que nos encontramos, e justamente, você quem me deu esse apoio e
+      acreditou em mim quando eu mesmo não acreditei \n Obrigado 12/01
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("test");
+}
+
+function test2() {
+  console.log("identação errada");
 }
 
 export default Home;
